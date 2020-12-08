6c5354b42cac0c43a69a661e87e6079f00477158
Moran1324 <67854443+Moran1324@users.noreply.github.com>
2020-12-08 15:05:09 +0200

    .env port udpated

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 const app = require('./app');
 
-const port = 8080;
+const PORT = process.env.PORT || 8080;
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
